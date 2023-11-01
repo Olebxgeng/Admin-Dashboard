9b02ec1f51eac74fa7ff03741ade8dd7531b6f97
Olebogeng Sebogodi <sebogodio@icloud.com>
2023-11-01 13:37:36 +0200

    JavaScript

---
+++ index.js
@@ -0,0 +1,19 @@
+const sideMenu = document.querySelector("aside")
+const menuBtn = document.querySelector("#menu-btn")
+const closeBtn = document.querySelector("#close-btn")
+const themeToggle = document.querySelector(".theme-toggle")
+
+menuBtn.addEventListener("click", () =>{
+    sideMenu.style.display = "block"
+})
+
+closeBtn.addEventListener("click", () =>{
+    sideMenu.style.display = "none"
+})
+
+themeToggle.addEventListener("click", () =>{
+    document.body.classList.toggle("dark-theme-variables")
+
+     themeToggle.querySelector("span:nth-child(1)").classlist.toggle("active")
+     themeToggle.querySelector("span:nth-child(2)").classlist.toggle("active")
+})
